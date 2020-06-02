be35aff727cd626363ca9c50e660d688c1085838
LucasSalu <samuel_6000@outlook.com>
2020-06-02 02:25:17 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,18 @@
 package application;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args){
 		
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
-		
-		
+		Seller seller = new Seller(19, "lucas", "Lucas@Gmail.com", new Date(), 1000.0, obj);
+		System.out.println(seller);
 	}
 
 }
